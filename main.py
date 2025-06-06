60ca85a9814e23f13bc2e1f00a1238b4bad33b83
bestworld65 <marekl0605@gmail.com>
2025-06-05 20:40:58 -0400

    parsing and find element in tree structure

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("index.html", "r") as f:
-    doc = BeautifulSoup(f, "html.parser")
+# HTML from File
+# with open("index.html", "r") as f:
+# 	doc = BeautifulSoup(f, "html.parser")
 
-print(doc.prettify())
+# tags = doc.find_all("p")[0]
+
+# print(tags.find_all("b"))
+
+# HTML from Website
+url = "https://www.newegg.ca/intel-core-i9-12th-gen-core-i9-12900k-alder-lake-lga-1700-desktop-cpu-processor/p/N82E16819118339?Item=N82E16819118339"
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+prices = doc.find_all(string="$")
+parent = prices[0].parent
+print(parent)
